564895fb1d345c4d5f59d5d838bd593e59cac75f
Fábio Carvalho <fabio.dscar@gmail.com>
2023-12-01 16:30:16 +0000

    Update packages. Fix theme switcher.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,30 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import expressiveCode from "astro-expressive-code";
+
+const astroExpressiveCodeOptions = {
+  themes: ["one-dark-pro", "vitesse-light"], // "one-dark-pro"
+  themeCssSelector: (theme) => {
+    if (theme.name == "vitesse-light")
+      return `[data-theme='autumn']`;
+    else if (theme.name == "one-dark-pro")
+      return `[data-theme='dracula']`;
+  },
+  customizeTheme: (theme) => {
+    if (theme.name == "one-dark-pro") {
+      theme.colors["tab.activeBorder"] = "#bd93f9";
+
+    } else if (theme.name == "vitesse-light") {
+      theme.colors["tab.activeBorderTop"] = "#d85251";
+    }
+
+    return theme;
+  }
+}
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fabio-dscar.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [expressiveCode(astroExpressiveCodeOptions), mdx(), sitemap(), tailwind()]
 });
